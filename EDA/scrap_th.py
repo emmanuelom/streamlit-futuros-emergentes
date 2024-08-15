62ebee4805cc637a22317fb326b330cc3ffdf99b
Emmanuel Ovalle <emmanuel.ovma@gmail.com>
2024-08-15 12:04:27 -0600

    Remove pre-preocesing files

--- EDA/scrap_th.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-URL = "https://www.trendhunter.com/report?ak=cr_3764e3c6095a42fb08e3a9c6d0ca1#idea=485683"
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, "html.parser")
-print(soup)
