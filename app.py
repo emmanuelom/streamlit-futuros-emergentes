5bbe64697e7270d1e6b3c5b9cda43c2ad70f23f0
Emmanuel Ovalle <emmanuel.ovma@gmail.com>
2024-08-19 18:44:45 +0000

    app updated

--- app.py
+++ app.py
@@ -180,16 +180,14 @@ if __name__ == "__main__":
 
     st.markdown(hide_header, unsafe_allow_html=True)
 
-    st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+    # Add custom CSS to hide the "Manage app" button
+    hide_manage_app_button = """
+        <style>
+        button[data-testid="manage-app-button"] {
+            display: none !important;
+        }
+        </style>
+        """
+
+    st.markdown(hide_manage_app_button, unsafe_allow_html=True)
 
