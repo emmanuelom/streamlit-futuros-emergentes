8d59a7cd2af006438400f19109143f608c16ad63
Emmanuel Ovalle <emmanuel.ovma@gmail.com>
2024-08-19 18:17:40 +0000

    App Updated

--- app.py
+++ app.py
@@ -168,3 +168,16 @@ if __name__ == "__main__":
     footer = """<style>.footer {position: fixed;left: 0;bottom: 0;width: 100%;background-color: #1d285c;color: white;text-align: center;}
     </style><div class='footer'><p>Universidad De La Salle Bajío - Dirección de Investigación y Doctorado &copy; Copyright 2024. <br /> Investigación de futuros: Prospectiva de tendencias científicas y tecnológicas en Guanajuato. Con clave IDEA GTO/SG/386/2023</p></div>"""
     st.markdown(footer, unsafe_allow_html=True)
+
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
